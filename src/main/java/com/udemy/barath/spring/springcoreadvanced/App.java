fb0727f03351d474698a5726f7bc84f47a2eef3b
Audra Wathen <mrsbluerose@gmail.com>
2021-04-16 15:01:20 -0600

    initial commit

---
+++ src/main/java/com/udemy/barath/spring/springcoreadvanced/App.java
@@ -0,0 +1,13 @@
+package com.udemy.barath.spring.springcoreadvanced;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
